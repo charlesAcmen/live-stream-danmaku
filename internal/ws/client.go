618c6215fe16d79538708792eb028b6a9ca9b416
charlesAcmen <15591542871@163.com>
2026-01-14 17:25:37 +0800

    client write and read via JSON

--- internal/ws/client.go
+++ internal/ws/client.go
@@ -1,6 +1,11 @@
 package ws
 
 import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/charlesAcmen/livestream-danmaku/internal/model" //database structures
 	"github.com/gorilla/websocket"
 )
 
@@ -24,16 +29,42 @@ func (c *Client) ReadPump(manager *Manager) {
 
 	for {
 		// 1. read message from WebSocket
-		_, message, err := c.Socket.ReadMessage()
+		_, messageBytes, err := c.Socket.ReadMessage()
 		if err != nil {
+			if websocket.IsUnexpectedCloseError(err,
+				websocket.CloseGoingAway,         // 1001: browser navigation leave/refreshing
+				websocket.CloseAbnormalClosure) { //1006:connection closed abnormally
+				//log when error does not belong to reasons mentioned above
+				log.Printf("[CLIENT]error: %v", err)
+			}
 			break // if read error, break the loop
 		}
-		// 2. send message to Manager's broadcast channel
-		manager.Broadcast <- message
+
+		// This ensures all downstream systems (Redis, Kafka) know WHO sent it.
+		msgObj := model.DanmakuMessage{
+			RoomID:     c.RoomID,
+			UserID:     c.UserID,
+			Username:   c.Username,
+			UserAvatar: c.UserAvatar,
+			Content:    string(messageBytes), // The actual text
+			//if sent successfully,set send time as accepting time
+			SendTime: time.Now(),
+		}
+
+		// 3. Serialize to JSON bytes
+		jsonBytes, err := json.Marshal(msgObj)
+		if err != nil {
+			log.Println("[CLIENT]JSON Marshal Error:", err)
+			continue
+		}
+
+		// 2. send the JSON bytes to Manager's broadcast channel
+		manager.Broadcast <- jsonBytes
 	}
 }
 
 // WritePump: listen from Send channel, once there is a message, write to WebSocket
+// i.e pumps messages from the hub to the websocket connection
 func (c *Client) WritePump() {
 	defer func() {
 		c.Socket.Close()
@@ -41,6 +72,7 @@ func (c *Client) WritePump() {
 
 	for {
 		// get message from Send channel
+		// Note: 'message' here is already a JSON string coming from Redis/Manager.
 		message, ok := <-c.Send
 		if !ok {
 			// channel is closed
@@ -48,7 +80,11 @@ func (c *Client) WritePump() {
 			return
 		}
 
+		//Note: could use NextWriter here to enable write in stream manner
+		//avoiding for too many times when huge wave of danmaku coming
+
 		// write to WebSocket
 		c.Socket.WriteMessage(websocket.TextMessage, message)
+
 	}
 }
