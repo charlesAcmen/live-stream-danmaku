fa65da18d504c0a4ec01798b52944358386baa7e
charlesAcmen <15591542871@163.com>
2026-01-21 10:55:23 +0800

    simplified room structure

--- internal/model/room.go
+++ internal/model/room.go
@@ -1,13 +1,6 @@
 package model
 
-import "time"
-
 type Room struct {
-	ID          uint   `gorm:"primaryKey;autoIncrement"`
-	RoomNumber  string `gorm:"type:varchar(20);uniqueIndex;not null"`
-	StreamerID  uint   `gorm:"index;not null"`             // user id of streamer
-	Title       string `gorm:"type:varchar(100);not null"` // title of stream
-	OnlineCount int    `gorm:"default:0"`                  // snapshot value of online audients,real value is stored in redis
-	IsLive      bool   `gorm:"default:false;index"`
-	CreatedAt   time.Time
+	ID         uint64 `gorm:"primaryKey;autoIncrement"`
+	RoomNumber string `gorm:"type:varchar(20);uniqueIndex;not null"`
 }
